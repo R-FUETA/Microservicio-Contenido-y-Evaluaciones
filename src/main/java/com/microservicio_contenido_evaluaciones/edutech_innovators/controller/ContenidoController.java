bd757a5e1299d69c3315af15547bc83cca02904b
Vina del Mar <vina@duoc.acad>
2025-06-23 14:40:59 -0400

    1.0.3

--- src/main/java/com/microservicio_contenido_evaluaciones/edutech_innovators/controller/ContenidoController.java
+++ src/main/java/com/microservicio_contenido_evaluaciones/edutech_innovators/controller/ContenidoController.java
@@ -2,6 +2,10 @@ package com.microservicio_contenido_evaluaciones.edutech_innovators.controller;
 
 import com.microservicio_contenido_evaluaciones.edutech_innovators.model.Contenido;
 import com.microservicio_contenido_evaluaciones.edutech_innovators.service.ContenidoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,36 +13,37 @@ import java.util.List;
 
 @RestController // Marca esta clase como un controlador REST
 @RequestMapping("/api/contenidos") // Ruta base para todos los endpoints relacionados con Contenido
+@Tag(name = "Contenidos", description = ("Permite eliminar, crear y mostrar Contenidos"))
 public class ContenidoController {
 
     @Autowired
     private ContenidoService contenidoService; // Inyección del servicio para manejar lógica de negocio
-
+    @Operation(summary = "Crear Contenido", description = "Permite Crear un nuevo contenido")
     @PostMapping // Endpoint para crear un nuevo contenido
     public Contenido crearContenido(@RequestBody Contenido contenido) {
         return contenidoService.crearContenido(contenido);
     }
-
+    @Operation(summary = "Actualizar Contenido", description = "Permite Actualizar Contenido")
     @PutMapping("/{contenidoId}") // Endpoint para actualizar un contenido existente
     public Contenido actualizarContenido(@PathVariable Long contenidoId, @RequestBody Contenido contenido) {
         return contenidoService.actualizarContenido(contenidoId, contenido);
     }
-
+    @Operation(summary = "Eliminar Contenido", description = "Permite Eliminar Contenido Ingresado por ID")
     @DeleteMapping("/{contenidoId}") // Endpoint para eliminar un contenido por su ID
     public void eliminarContenido(@PathVariable Long contenidoId) {
         contenidoService.eliminarContenido(contenidoId);
     }
-
+    @Operation(summary = "Obtener Contenido", description = "Permite Obtener Contenido por su ID")
     @GetMapping("/{contenidoId}") // Endpoint para obtener un contenido por su ID
     public Contenido obtenerPorId(@PathVariable Long contenidoId) {
         return contenidoService.obtenerPorId(contenidoId);
     }
-
+    @Operation(summary = "Lista de Contenidos", description = "Permite Visualizar Todos los contenidos registrados y los contenidos ya ingresados anteriormente")
     @GetMapping // Endpoint para listar todos los contenidos
     public List<Contenido> listarContenidos() {
         return contenidoService.listarContenidos();
     }
-
+    @Operation(summary = "Lista de Contenidos por ID de curso", description = "Permite Visualizar todos los contenidos segun el ID del curso asociado")
     @GetMapping("/curso/{cursoId}") // Endpoint para listar contenidos según el ID del curso asociado
     public List<Contenido> listarPorCurso(@PathVariable Long cursoId) {
         return contenidoService.listarPorCursoId(cursoId);
--- src/main/java/com/microservicio_contenido_evaluaciones/edutech_innovators/controller/EvaluacionController.java
+++ src/main/java/com/microservicio_contenido_evaluaciones/edutech_innovators/controller/EvaluacionController.java
@@ -2,6 +2,10 @@ package com.microservicio_contenido_evaluaciones.edutech_innovators.controller;
 
 import com.microservicio_contenido_evaluaciones.edutech_innovators.model.Evaluacion;
 import com.microservicio_contenido_evaluaciones.edutech_innovators.service.EvaluacionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,42 +13,50 @@ import java.util.List;
 
 @RestController // Marca esta clase como un controlador REST
 @RequestMapping("/api/evaluaciones") // Ruta base para todos los endpoints de evaluaciones
+@Tag(name = "Evaluaciones", description = ("Permite crear, eliminar, modificar y listar evaluacines"))
 public class EvaluacionController {
 
     @Autowired
     private EvaluacionService evaluacionService; // Inyección del servicio de evaluaciones
 
     @PostMapping // Crear nueva evaluación
+    @Operation(summary = "Crear Evaluacion", description = "Permite Crear una Nueva Evaluacion")
     public Evaluacion crearEvaluacion(@RequestBody Evaluacion evaluacion) {
         return evaluacionService.crearEvaluacion(evaluacion);
     }
 
     @PutMapping("/{evaluacionId}") // Actualizar evaluación existente
+    @Operation(summary = "Actualizar Evaluacion", description = "Permite actualizar evaluacion existentes")
     public Evaluacion actualizarEvaluacion(@PathVariable Long evaluacionId, @RequestBody Evaluacion evaluacion) {
         return evaluacionService.actualizarEvaluacion(evaluacionId, evaluacion);
     }
 
     @DeleteMapping("/{evaluacionId}") // Eliminar evaluación por ID
+    @Operation(summary = "Eliminar Evaluacion por ID", description = "Permite Eliminar Evaluacion Por su ID ")
     public void eliminarEvaluacion(@PathVariable Long evaluacionId) {
         evaluacionService.eliminarEvaluacion(evaluacionId);
     }
 
     @GetMapping("/{evaluacionId}") // Obtener evaluación por ID
+    @Operation(summary = "Obtener Evaluacion por ID", description = "Permite obtener evaluacion por ID")
     public Evaluacion obtenerPorId(@PathVariable Long evaluacionId) {
         return evaluacionService.obtenerPorId(evaluacionId);
     }
 
     @GetMapping // Listar todas las evaluaciones
+    @Operation(summary = "Lista de todas las Evaluaciones", description = "Permite listar todas las evaluacioens")
     public List<Evaluacion> listarEvaluaciones() {
         return evaluacionService.listarEvaluaciones();
     }
 
     @GetMapping("/curso/{cursoId}") // Listar evaluaciones por curso
+    @Operation(summary = "Lista todas las evaluaciones por curso", description = "Permite Listar todas las evaluaciones por curso")
     public List<Evaluacion> listarPorCurso(@PathVariable Long cursoId) {
         return evaluacionService.listarPorCursoId(cursoId);
     }
-
+    
     @GetMapping("/contenido/{contenidoId}") // Listar evaluaciones por contenido
+    @Operation(summary = "Lista Evaluaciones por contenido", description = "Permite Listar evaluaciones por contenido creado")
     public List<Evaluacion> listarPorContenido(@PathVariable Long contenidoId) {
         return evaluacionService.listarPorContenidoId(contenidoId);
     }
